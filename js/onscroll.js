2905205f5ec101dc7b10e40c970af2052ec13bd1
Yavonix <roman@greens.id.au>
2019-03-11 16:25:04 +0800

    Added unused javascript

--- js/onscroll.js
+++ js/onscroll.js
@@ -8,3 +8,28 @@ $(document).on("scroll", function () {
 		$("header").removeClass("transparent");
 	}
 });
+
+
+
+
+
+
+//console.log("start");
+// $(document).ready(function() {
+// 	setInterval(function(){
+// 			console.log("1");
+//
+// 			document.getElementById('parra').style.backgroundImage = 'url(img01.jpg)';
+//
+//
+// 			setTimeout(function(){
+// 			console.log("2");
+//
+//
+// 			document.getElementById('parra').style.backgroundImage = 'url(img02.jpg)';
+// 
+//
+//
+// 	}, 8000);
+// }, 16000);
+// });
