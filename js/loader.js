1ec5c10b2e12429bfb77eea897cd9fd4d5595a20
Namor888 <37795234+Namor888@users.noreply.github.com>
2019-04-17 22:42:48 +0800

    Only shows scrollbar once loaded.
    Only shows scrollbar once loaded.

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,15 @@
+function ShowScroll() {
+  $('html').css({'overflow-y':'visible'});
+}
+
+
+
 $(window).on('load', function() {
 
   $('#status').fadeOut();
   $('#preloader').delay(350).fadeOut('slow');
   $('body').delay(350).css({'overflow':'visible'});
-  
+
+  setTimeout(ShowScroll, 700)
+
 })
