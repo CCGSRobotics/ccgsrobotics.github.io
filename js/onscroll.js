f0827e185943f18b2d7f2a361ffd14de3bf98e07
Namor888 <37795234+Namor888@users.noreply.github.com>
2019-06-22 17:28:31 +0800

    Fixed bug

--- js/onscroll.js
+++ js/onscroll.js
@@ -1,5 +1,5 @@
 $(document).on("scroll", function () {
-	if ($(document).scrollTop() > 750) { //Adjust value to fit scroll properly
+	if ($(document).scrollTop() > ($(window).height() - $(".cf").height())) { //Adjust value to fit scroll properly
 		if (large < 5) {
 		$("header").addClass("transparent");
 	}
