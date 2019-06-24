0bbdd3402a67cbccfe0f2087cb3eb98a6d7076f8
Namor888 <37795234+Namor888@users.noreply.github.com>
2019-06-24 21:59:03 +0800

    Added dynamic parralax

--- js/onscroll.js
+++ js/onscroll.js
@@ -7,3 +7,19 @@ $(document).on("scroll", function () {
 		$("header").removeClass("transparent");
 	}
 });
+
+
+$(window).on('load', function() {
+  var parallax = $( ".parallax" ), speed = -0.6;
+
+  window.onscroll = function(){
+    [].slice.call(parallax).forEach(function(el,i){
+
+      var windowYOffset = window.pageYOffset,
+          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+
+      el.style.backgroundPosition = elBackgrounPos;
+
+    });
+  };
+});
